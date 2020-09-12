1722f2f8e2d11fec70a1c84357419e2747902359
manzaloros <zacharymansellcello@gmail.com>
2020-09-12 12:28:00 -0700

    Install eslint, start adding webpack

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello from the React App!</div>
+    )
+  }
+}
+
+export default App;
